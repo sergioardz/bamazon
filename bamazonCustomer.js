b757f958f7ab5d507f0c0afba07a9ee0fe1579ad
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-05-23 14:12:00 -0500

    Customer App - list of items is properly displayed in the console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,9 +17,38 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 });
 
+// Test Connection and Welcome message!
 connection.connect(function (err) {
 	if (err) throw err;
-	console.log("connected as id " + connection.threadId);
+	console.log(
+		"=====================================================" +
+		"\nWelcome to Bamazon!, These are the available items: " +
+		"\n=====================================================");
+	displayItems();
 	connection.end();
 });
 
+
+// First Function that displays all product information, including a header and calling the second function at the end for the next steps
+var displayItems = function () {
+	connection.query("SELECT * FROM products", function (err, response) {
+		if (err) throw err;
+		console.log(
+			"ID || Product || Department || Price || Stock " +
+			"\n-----------------------------------------------------"
+		);
+		for (i = 0; i < response.length; i++) {
+			console.log(
+				response[i].item_id + " || " +
+				response[i].product_name + " || " +
+				response[i].department_name + " || " +
+				response[i].price + " || " +
+				response[i].stock_quantity + "\n"
+			);
+		}
+		// promptUser();
+	});
+};
+
+
+// Second Function that will ask the user which id belongs to the product they want to purchase and a second prompt to ask for the quantity to purchase of the selected item
